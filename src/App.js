718a674ea0afbeac5206b3eb775a8b071184cd8f
Randy Rivera <randyr7k@gmail.com>
2021-08-12 16:33:43 -0400

    Reuse my first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import ExpensItem from "./components/ExpenseItem";
-
-// function App() {
-//     return (
-//         <div>
-//             <h2> Let 's get started!</h2>
-//             <p> This is also visible </p>
-//         </div>
-//     );
-// }
-
+import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
-    return (
-        <div>
-            <h2> Let 's get started!</h2>
-            <p> This is also visible </p>
-            <ExpensItem></ExpensItem>
-        </div>
-    );
-}
+  const expenses = [
+    {
+      id: 1,
+      title: "Toilet paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      title: "Car insurance",
+      amount: 399.9,
+      date: new Date(2020, 5, 8),
+    },
+    {
+      id: 3,
+      title: "New Cellphone",
+      amount: 999.99,
+      date: new Date(2020, 1, 1),
+    },
+    {
+      id: 4,
+      title: "Laptop",
+      amount: 1400.6,
+      date: new Date(2021, 7, 4),
+    },
+  ];
+
+  return (
+    <div>
+      <ExpenseItem
+        id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
-const ExpensItem = () => {
-    return <h2>Expense item here!</h2>
-}
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-export default ExpensItem;
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
