a63116cc48c6a2b102dde5421e9f9d5815115faf
Randy Rivera <randyr7k@gmail.com>
2021-08-12 12:00:49 -0400

    Creat my first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpensItem from "./components/ExpenseItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//     return (
+//         <div>
+//             <h2> Let 's get started!</h2>
+//             <p> This is also visible </p>
+//         </div>
+//     );
+// }
+
+
+const App = () => {
+    return (
+        <div>
+            <h2> Let 's get started!</h2>
+            <p> This is also visible </p>
+            <ExpensItem></ExpensItem>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+const ExpensItem = () => {
+    return <h2>Expense item here!</h2>
+}
+
+export default ExpensItem;
