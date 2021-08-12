eda78dc144a8f43852fa2d3ede70b0c1940cbacc
Randy Rivera <randyr7k@gmail.com>
2021-08-12 18:23:00 -0400

    Place expense items in expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 const App = () => {
   const expenses = [
@@ -30,30 +30,7 @@ const App = () => {
 
   return (
     <div>
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses expenses={expenses} />
     </div>
   );
 };
---
+++ src/components/Expenses.js
@@ -0,0 +1,36 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  const expenses = props.expenses;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+};
+
+export default Expenses;
