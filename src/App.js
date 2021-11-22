2ea94ac5d7ba8fbf683b88be8c10eae54a3ec3c3
Randy Rivera <randyr7k@gmail.com>
2021-11-22 12:23:46 -0400

    Clear code from section 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,8 +29,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,23 @@
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter"
 
 const Expenses = (props) => {
   const expenses = props.expenses;
+
+  const [enteredYear, setEnteredYear] = useState('2020');
+
+  const filterExpenseData = selectedYear => {
+    // console.log("Expenses.js");
+    // console.log(selectedYear);
+    setEnteredYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={enteredYear} onFilterExpenseData={filterExpenseData}/>
       <ExpenseItem
         id={expenses[0].id}
         title={expenses[0].title}
